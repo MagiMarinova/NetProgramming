72dacfc2e5919464bf1d5c00716f78e1adb91bea
MagiMarinova <magi.marinowax@gmail.com>
2018-12-10 16:57:57 +0200

    Complete regexes

---
+++ input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /[A-Z][a-z]+/,
+"exercise_2": /088[1-9]{7}/,
+"exercise_3": /[^01]+/,
+"exercise_4": /^[a-zA-Z][\w.+]{2,13}$/,
+"exercise_5": /^(?:[0-9]|(?:[1-9][0-9])|(?:[1-9][0-9][0-9])|(?:[1-1][0-4][0-9][0-9])|(?:1500))$/,
+"exercise_6": /class=['"][\w+ ]+['"]/
+};
+cssSelectors = {
+"exercise_1": "css",
+"exercise_2": "css",
+"exercise_3": "css",
+"exercise_4": "css",
+"exercise_5": "css",
+"exercise_6": "css",
+"exercise_7": "css",
+"exercise_8": "css"
+};
